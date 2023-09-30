0b933ac311b93e85906b6acd645299ec0b2c88fc
Fan Yang <yangfanhewangzheng@gmail.com>
2023-09-30 01:30:48 -0400

    create shipIsBroken undefined

--- server.js
+++ server.js
@@ -3,6 +3,30 @@ const app = express()
 const port = 3000
 const jsxEngine = require('jsx-view-engine')
 
-app.get('/new', (req, res)=>{
-    res.render('new')
+app.set('view engine', "jsx")
+app.engine("jsx", jsxEngine())
+
+app.get('/', (req, res)=>{
+    res.send("Welcome to captains' log")
+})
+app.get('/logs', (req, res)=>{
+    res.send('captains')
+})
+//New
+app.get('/logs/new', (req, res)=>{
+    res.render('New')
+})
+//Create
+app.post('/logs', (req, res)=>{
+    // if(req.body.shipIsBroken === "on"){
+    //     req.body.shipIsBroken = true
+    // }else{
+    //     req.body.shipIsBroken = false
+    // }
+res.send(req.body)
+})
+
+
+app.listen(port,()=>{
+    console.log('listening')
 })
--- views/New.jsx
+++ views/New.jsx
@@ -4,12 +4,13 @@ class New extends React.Component {
     return (
       <div>
         <form action="/logs" method="POST">
-          <input type="text" name="title" /> <br />
-          <input type="textarea" name="entry" /> <br />
-          <input type="checkbox" name="shipIsBroken"/><br />
+          Title: <input type="text" name="title" /> <br />
+          Entry: <input type="textarea" name="entry" /> <br />
+          shipIsBroken: <input type="checkbox" name="shipIsBroken"/><br />
           <input type="submit" value="Creat a log" />
         </form>
       </div>
     );
   }
 }
+module.exports = New
